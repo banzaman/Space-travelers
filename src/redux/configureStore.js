bf744d7245b500476264fbab72d8d71207dbf62a
MARK RUBANZA <markrubanza10@gmail.com>
2023-04-11 15:09:40 +0300

    Fix linter errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,23 +1,22 @@
 import {
-    applyMiddleware, compose, legacy_createStore as createStore, combineReducers,
-  } from 'redux';
+  applyMiddleware, compose, legacy_createStore as createStore, combineReducers,
+} from 'redux';
 
-  import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 
-  import missionsReducer from './missions/Missions';
+import missionsReducer from './missions/Missions';
 
-  const rootReducer = combineReducers({
-    missions: missionsReducer,
-  });
+const rootReducer = combineReducers({
+  missions: missionsReducer,
+});
 
-  const initialState = {};
+const initialState = {};
 
-  const middleware = [thunk];
+const middleware = [thunk];
 
-  export default () => {
-    const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
-    return {
-      store,
-    };
+export default () => {
+  const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
+  return {
+    store,
   };
-  
+};
