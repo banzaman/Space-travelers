bbafc367f7ad7d8e758c99329e2ae64596edbf9e
MARK RUBANZA <markrubanza10@gmail.com>
2023-04-13 14:00:53 +0300

    Styling according to Layout

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,16 +3,22 @@ import { useSelector } from 'react-redux';
 const MyProfilePage = () => {
   const { rocketItems } = useSelector((store) => store.rockets);
   const reserveRockets = rocketItems.filter((rocket) => rocket.reserved === true);
+
   return (
-    <div>
-      <hr />
-      <section className="myRockets">
-        <h2>My Rockets</h2>
-        {reserveRockets.map((rockets) => (
-          <li key={rockets.id}>
-            {rockets.name }
-          </li>
-        ))}
+    <div className="profile-section">
+      <section className="profile-missions">
+        <h1>My Rockets</h1>
+        <ul>
+          {reserveRockets.length > 0 ? (
+            reserveRockets.map((rocket) => (
+              <li key={rocket.id} className="profile-missions-item">
+                {rocket.name}
+              </li>
+            ))
+          ) : (
+            <li>No rockets reserved.</li>
+          )}
+        </ul>
       </section>
     </div>
   );
