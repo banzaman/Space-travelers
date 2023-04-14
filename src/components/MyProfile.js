d2146291f09d327ed8462b6cadc09e5a6fbb8fd7
Muhammed Berisso <muhammedb8826@gmail.com>
2023-04-14 06:38:51 +0300

    Resolved Conflicts

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import {
   Card, Col, Container, ListGroup, Row,
@@ -7,12 +6,19 @@ import { useSelector } from 'react-redux';
 
 export default function MyProfile() {
   const { missions } = useSelector((state) => state.missions);
-  const [reserved, setReserved] = useState([]);
+  const [reservedMissions, setReservedMission] = useState([]);
+  const { rocketItems } = useSelector((state) => state.rockets);
+  const [reservedRockets, setReservedRocket] = useState([]);
 
   useEffect(() => {
     const temp = missions.filter((mission) => mission.reserved);
-    setReserved(temp);
+    setReservedMission(temp);
   }, [missions]);
+
+  useEffect(() => {
+    const temp = rocketItems.filter((rocket) => rocket.reserved);
+    setReservedRocket(temp);
+  }, [rocketItems]);
 
   return (
     <Container>
@@ -21,7 +27,7 @@ export default function MyProfile() {
           <h2>My Missions</h2>
           <Card>
             <ListGroup variant="flush">
-              {reserved.map((mission) => (
+              {reservedMissions.map((mission) => (
                 <ListGroup.Item key={mission.mission_id}>
                   {mission.mission_name}
                 </ListGroup.Item>
@@ -32,36 +38,16 @@ export default function MyProfile() {
         <Col>
           <h2>My Rockets</h2>
           <Card>
-            <ListGroup />
+            <ListGroup variant="flush">
+              {reservedRockets.map((rocket) => (
+                <ListGroup.Item key={rocket.id}>
+                  {rocket.name}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
           </Card>
         </Col>
       </Row>
     </Container>
-
-import { useSelector } from 'react-redux';
-
-const MyProfilePage = () => {
-  const { rocketItems } = useSelector((store) => store.rockets);
-  const reserveRockets = rocketItems.filter((rocket) => rocket.reserved === true);
-
-  return (
-    <div className="profile-section">
-      <section className="profile-missions">
-        <h1>My Rockets</h1>
-        <ul>
-          {reserveRockets.length > 0 ? (
-            reserveRockets.map((rocket) => (
-              <li key={rocket.id} className="profile-missions-item">
-                {rocket.name}
-              </li>
-            ))
-          ) : (
-            <li>No rockets reserved.</li>
-          )}
-        </ul>
-      </section>
-    </div>
   );
-};
-
-export default MyProfilePage;
+}
